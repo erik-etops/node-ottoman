a89f8354f8e29023a02a6eafa33a9632e654570e
Moxious <moxious@oldhat.org>
2016-06-16 15:18:37 -0400

    refactored _makeField from ottoman to schema, added tests

--- test/schema.test.js
+++ test/schema.test.js
@@ -1,4 +1,6 @@
 'use strict';
+var H = require('./harness');
+var ottoman = H.lib;
 
 var assert = require('assert');
 var Schema = require('../lib/schema');
@@ -10,4 +12,45 @@ it('should provide a custom inspector for CoreTypes', function () {
 it('should provide a custom inspector for ModelRefs', function () {
   var testRef = new Schema.ModelRef('Test');
   assert.equal(testRef.inspect(), 'ModelRef(Test)');
+});
+
+describe('Dynamic schema capabilities', function () {
+  it('should allow model field inspection', function (done) {
+    var modelId = H.uniqueId('model');
+    var TestMdl = ottoman.model(modelId, {
+      name: 'string',
+      x: 'number',
+      doc: {
+        subdoc: {
+          deeper: {
+            inception: {
+              y: 'boolean'
+            }
+          }
+        }
+      }
+    });
+
+    var nameType = TestMdl.schema.field('name');
+    var subdocType = TestMdl.schema.field('doc.subdoc');
+    var yType = TestMdl.schema.field('doc.subdoc.deeper.inception.y');
+
+    assert.equal(nameType.type, Schema.StringType);
+    assert.equal(subdocType.type instanceof Schema.FieldGroup, true);
+    assert.equal(yType.type, Schema.BooleanType);
+    assert.equal(TestMdl.schema.field('NONEXISTANT'), null);
+
+    done();
+  });
+
+  it('should allow adding fields after the fact', function (done) {
+    var modelId = H.uniqueId('model');
+    var TestMdl = ottoman.model(modelId, {
+      name: 'string'
+    });
+
+    TestMdl.schema.addField({ name: 'added', type: 'string' });
+    assert.equal(TestMdl.schema.field('added').type, Schema.StringType);
+    done();
+  });
 });
